ca99d62da9b514e5c3fe89a30fa130ccb187d5f1
person808 <kainaluh808@gmail.com>
2015-05-21 20:18:07 -1000

    Remove oh-my-fish.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,21 +1,3 @@
-# Load oh-my-fish
-# Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
-
-# Theme
-set fish_theme cmorrell.com
-
-# All built-in plugins can be found at ~/.oh-my-fish/plugins/
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Enable plugins by adding their name separated by a space to the line below.
-set fish_plugins balias theme xdg
-
-# Path to your custom folder (default path is ~/.oh-my-fish/custom)
-#set fish_custom $HOME/dotfiles/oh-my-fish
-
-# Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
-
 # Load aliases
 if [ -f $HOME/.config/fish/aliases.fish ]
 	source $HOME/.config/fish/aliases.fish
