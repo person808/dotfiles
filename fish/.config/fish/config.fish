55f1d789dcf2d1c17d6a4990078fc96b60a18c12
person808 <kainaluh808@gmail.com>
2015-09-16 19:22:34 -1000

    Add rvm config to fish.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,7 +3,9 @@ if [ -f $HOME/.config/fish/aliases.fish ]
 	source $HOME/.config/fish/aliases.fish
 end
 
-
 set -U fish_greeting ""  # Hide fish greeting
 set -U fish_user_paths ~/.dotfiles/bin ~/.local/bin  # Set PATH
 set -gx EDITOR emacs
+set -gx BROWSER google-chrome-stable
+
+rvm default
