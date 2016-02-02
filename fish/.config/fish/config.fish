fde526cc756a3a7cc1af93a1f6c6d0acb2126081
person808 <kainaluh808@gmail.com>
2016-02-01 18:18:18 -1000

    Fix fish shell in emacs terminals.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,8 @@ if [ -f $HOME/.config/fish/aliases.fish ]
 	source $HOME/.config/fish/aliases.fish
 end
 
+function fish_title; end
+
 set -U fish_greeting ""  # Hide fish greeting
 set -U fish_user_paths ~/.dotfiles/bin ~/.local/bin  # Set PATH
 set -gx EDITOR nvim
