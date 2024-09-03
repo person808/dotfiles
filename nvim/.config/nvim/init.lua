41d6e07bb9bbc71a35f2b7297e429dc0fee1e14b
Kainalu Hagiwara <kainaluh808@gmail.com>
2024-09-02 19:44:34 -0700

    Tweak nvim-cmp appearance.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -473,11 +473,13 @@ require("lazy").setup({
         formatting = {
           fields = { "kind", "abbr", "menu" },
           format = function(entry, vim_item)
-            local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+            local kind = require("lspkind").cmp_format({
+              mode = "symbol_text",
+            })(entry, vim_item)
             local strings = vim.split(kind.kind, "%s", { trimempty = true })
             kind.kind = " " .. (strings[1] or "") .. " "
-            kind.menu = "    (" .. (strings[2] or "") .. ")"
-
+            kind.menu = ""
+            kind.concat = kind.abbr
             return kind
           end,
         },
