7e2fc5084d80c6b77be6960bb7080d4495177f6d
Kainalu Hagiwara <kainaluh808@gmail.com>
2024-09-15 23:57:25 -0700

    More keybindings

--- nvim/.config/nvim/lua/plugins/barbar.lua
+++ nvim/.config/nvim/lua/plugins/barbar.lua
@@ -1,2 +1,5 @@
-vim.keymap.set("n", "H", ":BufferPrevious<CR>", { desc = "Previous buffer" })
-vim.keymap.set("n", "L", ":BufferNext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "H", ":BufferPrevious<CR>", { silent = true, desc = "Previous buffer" })
+vim.keymap.set("n", "L", ":BufferNext<CR>", { silent = true, desc = "Next buffer" })
+vim.keymap.set("n", "<leader>bc", ":BufferClose<CR>", { silent = true, desc = "Close buffer" })
+vim.keymap.set("n", "<leader>bc", ":BufferClose<CR>", { silent = true, desc = "Close buffer" })
+vim.keymap.set("n", "<leader>bC", ":BufferRestore<CR>", { silent = true, desc = "Restore buffer" })
