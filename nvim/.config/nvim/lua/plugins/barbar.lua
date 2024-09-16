447559bc08b319fdc0e9e53a598fcec747079bcc
Kainalu Hagiwara <kainaluh808@gmail.com>
2024-09-15 21:12:45 -0700

    Use barbar for bufferline.

---
+++ nvim/.config/nvim/lua/plugins/barbar.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "H", ":BufferPrevious<CR>", { desc = "Previous buffer" })
+vim.keymap.set("n", "L", ":BufferNext<CR>", { desc = "Next buffer" })
--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++
@@ -1,3 +0,0 @@
-require("bufferline").setup()
-vim.keymap.set("n", "H", ":BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
-vim.keymap.set("n", "L", ":BufferLineCycleNext<CR>", { desc = "Next buffer" })
