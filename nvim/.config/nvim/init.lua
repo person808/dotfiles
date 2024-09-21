ff3b570e6b483efb3bdbb004ac4a78a223bfbe68
Kainalu Hagiwara <kainaluh808@gmail.com>
2024-09-20 22:01:35 -0700

    Update

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -25,6 +25,10 @@ vim.opt.autoread = true
 vim.opt.swapfile = false
 vim.g.mapleader = " "
 
+vim.g.clever_f_smart_case = true
+vim.g.clever_f_fix_key_direction = true
+vim.g.rooter_resolve_links = true
+
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
@@ -41,103 +45,100 @@ vim.keymap.set("n", "<leader>cX", vim.diagnostic.setloclist, { desc = "Buffer Di
 -- (happens when dropping a file on gvim) and for a commit message (it's
 -- likely a different one than last time).
 vim.api.nvim_create_autocmd("BufReadPost", {
-  group = vim.api.nvim_create_augroup("JumpToLastPosition", {}),
-  callback = function(args)
-    local valid_line = vim.fn.line([['"]]) >= 1 and vim.fn.line([['"]]) < vim.fn.line("$")
-    local not_commit = vim.b[args.buf].filetype ~= "commit"
+	group = vim.api.nvim_create_augroup("JumpToLastPosition", {}),
+	callback = function(args)
+		local valid_line = vim.fn.line([['"]]) >= 1 and vim.fn.line([['"]]) < vim.fn.line("$")
+		local not_commit = vim.b[args.buf].filetype ~= "commit"
 
-    if valid_line and not_commit then
-      vim.cmd([[normal! g`"]])
-    end
-  end,
+		if valid_line and not_commit then
+			vim.cmd([[normal! g`"]])
+		end
+	end,
 })
 
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-  group = vim.api.nvim_create_augroup("highlight_yank", {}),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	group = vim.api.nvim_create_augroup("highlight_yank", {}),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 vim.diagnostic.config({
-  virtual_text = false,
-  signs = true,
-  underline = true,
-  update_in_insert = false,
-  severity_sort = true,
-  float = {
-    border = "rounded",
-  },
+	virtual_text = false,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
+	float = {
+		border = "rounded",
+	},
 })
 
 local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
 for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
 vim.api.nvim_create_autocmd("CursorHold", {
-  group = vim.api.nvim_create_augroup("diagnostic_hover", {}),
-  callback = function()
-    local opts = {
-      focusable = false,
-      close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-      prefix = " ",
-      scope = "cursor",
-    }
-    vim.diagnostic.open_float(nil, opts)
-  end,
+	group = vim.api.nvim_create_augroup("diagnostic_hover", {}),
+	callback = function()
+		local opts = {
+			focusable = false,
+			close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+			prefix = " ",
+			scope = "cursor",
+		}
+		vim.diagnostic.open_float(nil, opts)
+	end,
 })
 
 do
-  -- Specifies where to install/use rocks.nvim
-  local install_location = vim.fs.joinpath(vim.fn.stdpath("data"), "rocks")
-  -- Set up configuration options related to rocks.nvim (recommended to leave as default)
-  local rocks_config = {
-    rocks_path = vim.fs.normalize(install_location),
-  }
-  vim.g.rocks_nvim = rocks_config
+	-- Specifies where to install/use rocks.nvim
+	local install_location = vim.fs.joinpath(vim.fn.stdpath("data"), "rocks")
+	-- Set up configuration options related to rocks.nvim (recommended to leave as default)
+	local rocks_config = {
+		rocks_path = vim.fs.normalize(install_location),
+	}
+	vim.g.rocks_nvim = rocks_config
 
-  -- Configure the package path (so that plugin code can be found)
-  local luarocks_path = {
-    vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?.lua"),
-    vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?", "init.lua"),
-  }
-  package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
+	-- Configure the package path (so that plugin code can be found)
+	local luarocks_path = {
+		vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?.lua"),
+		vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?", "init.lua"),
+	}
+	package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
 
-  -- Configure the C path (so that e.g. tree-sitter parsers can be found)
-  local luarocks_cpath = {
-    vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.so"),
-    vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.so"),
-  }
-  package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
+	-- Configure the C path (so that e.g. tree-sitter parsers can be found)
+	local luarocks_cpath = {
+		vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.so"),
+		vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.so"),
+	}
+	package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
 
-  -- Load all installed plugins, including rocks.nvim itself
-  vim.opt.runtimepath:append(
-    vim.fs.joinpath(rocks_config.rocks_path, "lib", "luarocks", "rocks-5.1", "rocks.nvim", "*")
-  )
+	-- Load all installed plugins, including rocks.nvim itself
+	vim.opt.runtimepath:append(
+		vim.fs.joinpath(rocks_config.rocks_path, "lib", "luarocks", "rocks-5.1", "rocks.nvim", "*")
+	)
 end
 
 -- If rocks.nvim is not installed then install it!
 if not pcall(require, "rocks") then
-  local rocks_location = vim.fs.joinpath(vim.fn.stdpath("cache"), "rocks.nvim")
-  if not vim.uv.fs_stat(rocks_location) then
-    -- Pull down rocks.nvim
-    vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "https://github.com/nvim-neorocks/rocks.nvim",
-      rocks_location,
-    })
-  end
+	local rocks_location = vim.fs.joinpath(vim.fn.stdpath("cache"), "rocks.nvim")
+	if not vim.uv.fs_stat(rocks_location) then
+		-- Pull down rocks.nvim
+		vim.fn.system({
+			"git",
+			"clone",
+			"--filter=blob:none",
+			"https://github.com/nvim-neorocks/rocks.nvim",
+			rocks_location,
+		})
+	end
 
-  -- If the clone was successful then source the bootstrapping script
-  assert(
-    vim.v.shell_error == 0,
-    "rocks.nvim installation failed. Try exiting and re-entering Neovim!"
-  )
-  vim.cmd.source(vim.fs.joinpath(rocks_location, "bootstrap.lua"))
-  vim.fn.delete(rocks_location, "rf")
+	-- If the clone was successful then source the bootstrapping script
+	assert(vim.v.shell_error == 0, "rocks.nvim installation failed. Try exiting and re-entering Neovim!")
+	vim.cmd.source(vim.fs.joinpath(rocks_location, "bootstrap.lua"))
+	vim.fn.delete(rocks_location, "rf")
 end
--- nvim/.config/nvim/lua/plugins/clever-f.lua
+++
@@ -1,2 +0,0 @@
-vim.g.clever_f_smart_case = true
-vim.g.clever_f_fix_key_direction = true
