ff2a67277e36d882f4bbec205b88f8aead15958a
Kainalu Hagiwara <kainaluh808@gmail.com>
2024-09-28 14:43:22 -0700

    Statusline config

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,6 +1,26 @@
 require("lualine").setup({
   options = {
     theme = "ayu",
+    section_separators = "",
+    component_separators = "",
+    globalstatus = true,
   },
-  extensions = { "lazy", "oil" },
+  sections = {
+    lualine_a = {
+      {
+        "mode",
+        fmt = function(str)
+          return str:sub(1, 1)
+        end,
+      },
+    },
+    lualine_b = {
+      "branch",
+      "diff",
+    },
+    lualine_c = {
+      { "diagnostics", symbols = { error = "● ", warn = "● ", hint = "● ", info = "● " } },
+    },
+  },
+  extensions = { "lazy", "oil", "quickfix" },
 })
