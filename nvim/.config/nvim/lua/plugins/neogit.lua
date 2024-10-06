58f97225856b20e89752b40fb670090b3dc71981
Kainalu Hagiwara <kainaluh808@gmail.com>
2024-10-06 12:41:38 -0700

    Hide foldcolumn in Neogit.

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -20,3 +20,12 @@ end, { desc = "Git commit" })
 vim.keymap.set("n", "<leader>gl", function()
   require("neogit").open({ "log" })
 end, { desc = "Git log" })
+
+local autocmd_group = vim.api.nvim_create_augroup("NeogitAutocmds", { clear = true })
+vim.api.nvim_create_autocmd({ "Filetype" }, {
+  group = autocmd_group,
+  pattern = { "Neogit*" },
+  callback = function()
+    vim.wo.foldcolumn = "0"
+  end,
+})
