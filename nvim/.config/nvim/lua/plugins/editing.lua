15095886b6eb4ad197841800849fab87a338638d
Kainalu Hagiwara <kainaluh808@gmail.com>
2025-09-07 19:28:33 -0700

    Update

--- nvim/.config/nvim/lua/plugins/editing.lua
+++ nvim/.config/nvim/lua/plugins/editing.lua
@@ -32,7 +32,7 @@ return {
 
           vim.wo.foldmethod = "expr"
           vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-          -- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
         end,
       })
     end,
